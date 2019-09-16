e0fa32467a7a03e38b06ce98f88bc983a34f1c00
Nguyễn Trường Giang <44118083+bingii98@users.noreply.github.com>
2019-09-17 04:14:18 +0700

    Push Bai 3  M2
    Update listView

---
+++ Tuan02_M2_Bai03/app/src/main/java/com/example/tuan02_m2_bai03/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.tuan02_m2_bai03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtName, lbKQ;
+    private Button btn;
+    private ListView lvXem;
+    private ArrayList<String> list;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = findViewById(R.id.txtName);
+        lbKQ = findViewById(R.id.lbKQ);
+        btn = findViewById(R.id.btn);
+        lvXem = findViewById(R.id.lvXem);
+
+        list = new ArrayList<>();
+
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        lvXem.setAdapter(adapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = txtName.getText().toString();
+                if(name.isEmpty()){
+                    txtName.setError("Tên không để rỗng");
+                }else {
+                    list.add(name);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        lvXem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                lbKQ.setText("Position: " + i + " - Value: " + adapterView.getItemAtPosition(i));
+            }
+        });
+    }
+}
